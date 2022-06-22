15afac32d6b850facc65debf06ade04480c63d0e
abhijhacodes <abhi.jha.cs@gmail.com>
2022-06-22 20:59:09 +0530

    linkedin link updated

--- data/Links.ts
+++ data/Links.ts
@@ -1,7 +1,6 @@
 export const BASE_URL = "https://www.abhijha.live/";
 export const GITHUB_URL = "https://github.com/abhijhacodes";
-export const LINKEDIN_URL =
-  "https://www.linkedin.com/in/abhishek-jha-3867b4205";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/abhijhacodes";
 export const EMAIL_URL = "mailto:abhi.jha.cs@gmail.com";
 export const INSTAGRAM_URL = "https://www.instagram.com/abhi.jha25/";
 export const TWITTER_URL = "https://twitter.com/abhitics";
