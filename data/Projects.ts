5a3cb392ec45398bba80a3aeb3dce7a9ac8648f7
abhijhacodes <abhishek.jha@setu.co>
2022-12-11 16:12:11 +0530

    project url typo fix

--- data/Projects.ts
+++ data/Projects.ts
@@ -157,7 +157,7 @@ export const ProjectsData: Array<ProjectProps> = [
     tags: ["Flutter", "Firebase"],
   },
   {
-    imageURL: "/images//project/rickmorty.png",
+    imageURL: "/images/project/rickmorty.png",
     category: ["All", "Web"],
     title: "Rick and Morty",
     description:
@@ -167,7 +167,7 @@ export const ProjectsData: Array<ProjectProps> = [
     tags: ["Next", "GraphQL", "ChakraUI"],
   },
   {
-    imageURL: "/images//project/terminal.png",
+    imageURL: "/images/project/terminal.png",
     category: ["All", "Web"],
     title: "Terminal style portfolio",
     description:
